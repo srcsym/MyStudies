7286f367fdde6fb2ce99019164f31301b20d0280
srcsym <62261809+srcsym@users.noreply.github.com>
2021-12-21 14:07:21 +0100

    Delete Codility_Exercises directory

--- Codility_Exercises/2_2_OddOccurrenceInArray(Arrays)/OddOccurencesInArray.py
+++
@@ -1,7 +0,0 @@
-def solution(A):
-    for item in A:
-        counter=A.count(item)
-
-        if counter%2!=0:
-            return item
-            break
--- Codility_Exercises/3_2_PermMissingElem(Time Complexity)/PermMissingElem.py
+++
@@ -1,6 +0,0 @@
-def solution(A):
-    n=len(A)+1
-    total=int((n*(n+1))/2)
-    return total-sum(A)
-
-print(solution([2,3,1,5]))
